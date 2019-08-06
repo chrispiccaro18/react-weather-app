1d2dd88cbc4c56cafce663337ffe7de419a8f069
chrispiccaro <chrispiccaro18@gmail.com>
2019-08-06 13:35:11 -0700

    adding header and styling to home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Home.css';
 import SearchForm from '../search/SearchForm';
 import AllForecasts from '../../containers/forecasts/AllForecasts';
+import Header from '../Header';
 
 export default function Home() {
 
   return (
     <main>
+      <Header />
       <SearchForm />
       <AllForecasts />
     </main>
